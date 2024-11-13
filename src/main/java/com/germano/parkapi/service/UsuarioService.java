5918caf72de59579da0dac885610d88e65ba1013
Germano <germano12344@gmail.com>
2024-11-13 09:39:02 -0300

    feat/create-user

--- src/main/java/com/germano/parkapi/service/UsuarioService.java
+++ src/main/java/com/germano/parkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.germano.parkapi.service;
 
+import com.germano.parkapi.entity.Usuario;
 import com.germano.parkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
