e62c964e36e95f77739fe7ffbb77ae3d62e8f3c3
Germano <germano12344@gmail.com>
2024-11-13 11:05:42 -0300

    refactor/usuarioDTO

---
+++ src/main/java/com/germano/parkapi/web/dto/UsuarioCreateDto.java
@@ -0,0 +1,10 @@
+package com.germano.parkapi.web.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class UsuarioCreateDto {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/germano/parkapi/web/dto/UsuarioResponseDto.java
@@ -0,0 +1,11 @@
+package com.germano.parkapi.web.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class UsuarioResponseDto {
+
+    private Long id;
+    private String username;
+    private String role;
+}
