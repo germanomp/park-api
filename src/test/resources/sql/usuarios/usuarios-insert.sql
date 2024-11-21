03dfbb65655fa990df37d17b1bc3ce527edc0cee
Germano <germano12344@gmail.com>
2024-11-21 10:12:19 -0300

    refactor/buscaporid status 200

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,3 @@
-insert into USUARIOS (id, username, password, role) values (100, 'ana@gmail.com', '123456', 'ROLE_ADMIN');
-insert into USUARIOS (id, username, password, role) values (101, 'bia@gmail.com', '123456', 'ROLE_CLIENTE');
-insert into USUARIOS (id, username, password, role) values (102, 'bob@gmail.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (100, 'ana@gmail.com', '$2a$12$34lzi2T8FTauon6UVDBwSeuDJVyALFgLCWZiG.TtxfwYYOuCbJBNi', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'bia@gmail.com', '$2a$12$34lzi2T8FTauon6UVDBwSeuDJVyALFgLCWZiG.TtxfwYYOuCbJBNi', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'bob@gmail.com', '$2a$12$34lzi2T8FTauon6UVDBwSeuDJVyALFgLCWZiG.TtxfwYYOuCbJBNi', 'ROLE_CLIENTE');
